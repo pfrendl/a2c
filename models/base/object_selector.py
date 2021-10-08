568f07586e5849434094cf31105b0bd52104a03b
dineNshine <pfre00@gmail.com>
2021-10-08 23:06:52 +0200

    object selector module simplified

--- models/base/object_selector.py
+++ models/base/object_selector.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -6,15 +8,13 @@ from torch import Tensor
 class ObjectSelector(nn.Module):
     def __init__(self, hidden_size: int, context_size: int, object_size: int):
         super().__init__()
+        self.hidden_size = hidden_size
+
         self.fc0 = nn.Linear(object_size, hidden_size)
         self.fc1 = nn.Linear(hidden_size, hidden_size)
 
         self.key_value = nn.Linear(hidden_size, 2 * hidden_size)
         self.query = nn.Linear(context_size, hidden_size)
-
-        self.gain = nn.Parameter(-0.5 * torch.tensor([[hidden_size]]).log())
-
-        self.hidden_size = hidden_size
 
     def forward(self, objects_list: list[Tensor], context: Tensor) -> tuple[Tensor, list[Tensor]]:
         split_sections = [len(obj) for obj in objects_list]
@@ -27,7 +27,7 @@ class ObjectSelector(nn.Module):
         key, value = self.key_value(x).chunk(2, dim=1)
         query = torch.repeat_interleave(self.query(context), split_sections_ten, dim=0)
 
-        logits = self.gain.exp() * (key * query).sum(dim=1, keepdim=True)
+        logits = (key * query).sum(dim=1, keepdim=True) / math.sqrt(self.hidden_size)
 
         logits_list = logits.split(split_sections, dim=0)
         values_list = value.split(split_sections, dim=0)
